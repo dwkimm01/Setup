0709891ffe7009f658a9a53bed9bd37bd902588e
Dave <dwkimm01@myfreebsd121.mydomain.loc>
2020-12-31 13:26:24 -0800

    Initial script commit for FreeBSD setup

---
+++ scripts/setup_freebsd.sh
@@ -0,0 +1,19 @@
+
+# TODO
+# add
+# kern.vty="vt" to /boot/loader.conf
+# pw group mod wheel -G `whoami`
+
+
+# Xorg
+sudo pkg install -y xorg
+sudo pkg install -y xfce xfce4-goodies
+sudo pkg install -y i3-gaps i3lock i3status dmenu xterm luit gnome-screenshot nitrogen
+sudo pkg install -y conky
+# Development
+sudo pkg install -y git
+sudo pkg install -y cmake 
+# Productivity
+sudo pkg install -y chromium
+# Extras
+sudo pkg install -y htop
