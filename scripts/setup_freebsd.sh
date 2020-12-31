30a4fb190203e737cbb1cfb11fd47906d5a744da
Dave <dwkimm01@myfreebsd121.mydomain.loc>
2020-12-31 13:34:41 -0800

    Update to add sudo and remove from each line

--- scripts/setup_freebsd.sh
+++ scripts/setup_freebsd.sh
@@ -4,16 +4,22 @@
 # kern.vty="vt" to /boot/loader.conf
 # pw group mod wheel -G `whoami`
 
+# System admin
+pkg install -y sudo
+pw group mod wheel -m dwkimm01
 
 # Xorg
-sudo pkg install -y xorg
-sudo pkg install -y xfce xfce4-goodies
-sudo pkg install -y i3-gaps i3lock i3status dmenu xterm luit gnome-screenshot nitrogen
-sudo pkg install -y conky
+pkg install -y xorg
+pkg install -y xfce xfce4-goodies
+pkg install -y i3-gaps i3lock i3status dmenu xterm luit gnome-screenshot nitrogen
+pkg install -y conky
+
 # Development
-sudo pkg install -y git
-sudo pkg install -y cmake 
+pkg install -y git
+pkg install -y cmake 
+
 # Productivity
-sudo pkg install -y chromium
+pkg install -y chromium
+
 # Extras
-sudo pkg install -y htop
+pkg install -y htop
